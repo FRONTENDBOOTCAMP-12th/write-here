e0eb8303ce71ca761fb596785ebc46e7620dc6a7
karrpp <sinceremin0718@gmail.com>
2025-03-05 13:50:54 +0900

    [Feat] 네비게이션 바 추가 및 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Landing from './pages/landing';
 import NotFound from './pages/not-found';
-
+import NavigationTest from './pages/navigation-test';
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/navigation-test" element={<NavigationTest />} />{' '}
+        {/*네비게이션 테스트 페이지 추가 */}
+      </Routes>
     </BrowserRouter>
   );
 }
