971a37ff1144513679ea26b9232cbf0bfc276141
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-03-06 16:29:47 +0900

    [Modify] Spring 컴포넌트
    - 경로 수정

--- src/components/Spring.tsx
+++ src/components/Spring.tsx
@@ -1,4 +1,4 @@
-import spring from '../../public/icons/spring.svg';
+import spring from '@/../public/icons/spring.svg';
 
 function Spring() {
   return <img src={spring} alt="" aria-hidden="true" width={22} height={40} />;
